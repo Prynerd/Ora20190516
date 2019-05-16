eedd59c4efeaa3ccb0e9431edd6d516c0a13616e
Prynerd <atti13@gmail.hu>
2019-05-16 12:02:45 +0200

    adds method

--- src/ora20190516/Ora20190516.java
+++ src/ora20190516/Ora20190516.java
@@ -2,6 +2,9 @@ package ora20190516;
 
 public class Ora20190516 {
 
+    public static void method(){
+        System.out.println("hello");
+    }
 
     public static void main(String[] args) {
         
