ee7650e0b9f0fb53fcadde96eb0ce7c9ef232bb0
Prynerd <atti13@gmail.hu>
2019-05-16 11:49:10 +0200

    new int[5]

--- src/ora20190516/Ora20190516.java
+++ src/ora20190516/Ora20190516.java
@@ -5,8 +5,10 @@ public class Ora20190516 {
 
     public static void main(String[] args) {
         
-        
-        
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
         
         
         
