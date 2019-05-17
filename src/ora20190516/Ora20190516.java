daed2f7bda1dde864dc66f69eafbe561c8717d52
Prynerd <atti13@gmail.hu>
2019-05-17 09:29:34 +0200

    newClass

--- src/ora20190516/Ora20190516.java
+++ src/ora20190516/Ora20190516.java
@@ -15,6 +15,9 @@ public class Ora20190516 {
 
     public static void main(String[] args) {
         
+        BranchClass bC = new BranchClass();
+        
+        
         int[] array = new int[5];
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
